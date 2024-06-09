fc0dfe189a0c56ef8744ad8fca02c0a00d9f474b
vamsi-Bade <107441229+vamsi-Bade@users.noreply.github.com>
2024-06-09 12:59:46 +0530

    Add files via upload

--- backend/controllers/notificationControllers.js
+++ backend/controllers/notificationControllers.js
@@ -3,31 +3,66 @@ const asyncHandler = require("express-async-handler");
 //@description     Fetch all notifications for a user
 //@route           GET /api/notification
 //@access          Protected
-const fetchNotification = asyncHandler(async (req, res) => {
-  try {
-    Notification.find({}).then((notifications) => {
-      res.json(notifications);
-    });
-  } catch (error) {
+const createNotification = asyncHandler(async (req, res) => {
+  const { notificationTitle, href, emailId } = req.body;
+
+  if (!notificationTitle || !href) {
     res.status(400);
-    throw new Error(error.message);
+    throw new Error("Please Enter all the Fields");
   }
-});
-
-const postNotification = asyncHandler(async (req, res) => {
   try {
-    const { notificationHeading, notificationDetails, pdf } = req.body;
-    const newNotification = new Notification({
-      notificationHeading,
-      notificationDetails,
-      pdf,
-    });
-    const savedNotification = await newNotification.save();
-    res.status(201).json(savedNotification);
-  } catch (error) {
-    console.error("Error adding notification:", error);
-    res.status(500).json({ error: "Failed to add notification" });
+    Notification.findOne({ emailId: req.body.emailId })
+      .then((notification) => {
+        if (notification) {
+          notification.notifications.push({
+            notificationTitle: notificationTitle,
+            href: href,
+          });
+
+          return notification.save();
+        } else {
+          const notificationData = {
+            notificationTitle: notificationTitle,
+            href: href,
+          };
+          return Notification.create({
+            emailId: emailId,
+            blogs: [notificationData],
+          });
+        }
+      })
+      .then((uNotification) => {
+        res.json(uNotification);
+      });
+  } catch (err) {
+    console.log(err);
+    res.json(err);
   }
 });
 
-module.exports = { fetchNotification, postNotification };
+const getNotification = asyncHandler(async (req, res) => {
+  const emailId = req.query.emailId;
+  try {
+    Notification.findOne({ emailId: emailId }).then((notification) =>
+      res.json(notification)
+    );
+  } catch (err) {
+    res.json(err);
+    console.log(err);
+  }
+});
+const deleteNotification = asyncHandler(async (req, res) => {
+  const { notificationId, emailId } = req.body;
+  try {
+    let notification = await Notification.findOne({ emailId: emailId });
+    notification.notifications.splice(notificationId + 1, 1);
+    notification.save();
+    res
+      .status(200)
+      .json({ success: true, message: "Notifications updated successfully" });
+  } catch (err) {
+    res.status(401);
+    throw new Error("Invalid Email or Password");
+  }
+});
+module.exports = { createNotification, deleteNotification, getNotification };
