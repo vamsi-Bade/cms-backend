d6de48ad965c092063b108df5b34d055fcee82b9
vamsi-Bade <107441229+vamsi-Bade@users.noreply.github.com>
2024-06-17 01:49:54 +0530

    Add files via upload

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,13 +3,17 @@ const {
   registerUser,
   authUser,
   allUsers,
+  getUsers,
+  editUser,
 } = require("../controllers/userControllers");
 const { protect } = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
-router.route("/").get(protect, allUsers);
+router.route("/").get(protect, getUsers);
+
 router.route("/").post(registerUser);
 router.post("/login", authUser);
+router.route("/editUser").put(protect, editUser);
 
 module.exports = router;
